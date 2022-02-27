abdae66156cb9abd237d1107e61ac332199c8825
Won <fain9301@yahoo.com>
2022-02-27 17:33:50 +0900

    feat: chapter02-04

---
+++ chapter02/chapter02-04/03.RefReturnThree/refReturnThree.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int refRefFuncTwo(int &ref) {
+    ref++;
+    return ref;
+}
+
+int main(int argc, char **argv) {
+    int num1 = 1;
+    int num2 = refRefFuncTwo(num1);
+
+    num1 += 1;
+    num2 += 100;
+
+    cout << "num1 : " << num1 << endl;
+    cout << "num2 : " << num2 << endl;
+    return 0;
+}
